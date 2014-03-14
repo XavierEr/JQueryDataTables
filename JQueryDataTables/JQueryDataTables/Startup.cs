b14dc6c03558a1bc3068c4931c2aacfb500a0e26
XavierEr <simpleton_er@hotmail.com>
2014-03-14 17:29:50 +0800

    Add JQueryDataTables solution

---
+++ JQueryDataTables/JQueryDataTables/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JQueryDataTables.Startup))]
+namespace JQueryDataTables
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
